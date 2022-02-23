2ee105e8f3406b5c39dbe7110393383a3c931e62
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-02-23 13:38:29 -0300

    Seeds.rb refactored to don't create if already exist

--- crypto_wallet/db/seeds.rb
+++ crypto_wallet/db/seeds.rb
@@ -6,20 +6,29 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-puts "Generating coins..."
-Coin.create!(
+spinner = TTY::Spinner.new("[:spinner] Generating coins... ")
+spinner.auto_spin 
+
+coins = [
+	{
     description: "Bitcoin",
     acronym: "BTC",
     url_image: "https://cdn.icon-icons.com/icons2/1487/PNG/512/8369-bitcoin_102502.png"
-)
-Coin.create!(
+	},
+	{
     description: "Ethereum",
     acronym: "ETH",
-    url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/480px-Ethereum-icon-purple.svg.png"
-)
-Coin.create!(
+    url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/"
+	},
+	{
     description: "Santos token",
     acronym: "SFC",
     url_image: "https://s2.coinmarketcap.com/static/img/coins/64x64/15248.png"
-)
-puts "Coins generated with success"
+	}
+]
+
+coins.each do |coin|
+	Coin.find_or_create_by!(coin)
+end
+
+spinner.success("(Coins generated with success)")
