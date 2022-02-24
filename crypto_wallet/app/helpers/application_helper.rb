ddd32840a639266d509ac5b6c311e03c3cbadd5c
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-02-24 14:27:53 -0300

    i18n gem

--- crypto_wallet/app/helpers/application_helper.rb
+++ crypto_wallet/app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
     
+    def locale
+        I18n.locale == :en ? "English-US" : "Português do Brasil"        
+    end
+
     def data_us(data)
         data.strftime("%m/%d/%Y") 
     end
