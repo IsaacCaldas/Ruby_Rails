ff3e6509f0273dabf474d3b3c67286581f87f749
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-02-24 14:46:00 -0300

    Cookies and Sessions

--- crypto_wallet/app/controllers/welcome_controller.rb
+++ crypto_wallet/app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:course] = "Course of Ruby on Rails [COOKIE]"
+    session[:course] = "Course of Ruby on Rails [SESSION]"
     @my_name = params[:name]
     @course = "Ruby on Rails"
   end
